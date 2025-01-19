77e6167b46a4c785070ac842bd2c46a82ad6c939
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-01-19 12:02:15 +0100

    init thread pool example

--- src/_08_Threads_Executors/Separation/Executor/ExecutorExample.java
+++ src/_08_Threads_Executors/Separation/Executor/ExecutorExample.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 
 public class ExecutorExample {
     public static void main(String[] args) {
-        Executor executor = Executors.newSingleThreadExecutor();
+        Executor executor = Executors.newSingleThreadExecutor(); // pula wątków - jeden wątek obsługuje wszystkie zadania w kolejności
         executor.execute(() -> System.out.println("Task in thread finished!"));
     }
 }
---
+++ src/_08_Threads_Executors/Separation/FixedThreadPoolExample/ExecutorExample.java
@@ -0,0 +1,4 @@
+package _08_Threads_Executors.Separation.FixedThreadPoolExample;
+
+public class ExecutorExample {
+}
