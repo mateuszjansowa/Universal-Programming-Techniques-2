e29f86c4bc6f91168f1109e1a6397a12d2e02acd
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-01-19 11:02:14 +0100

    add example annotation

---
+++ src/_06_Metadane/SprintBootAnnotationExample.java
@@ -0,0 +1,9 @@
+package _06_Metadane;
+
+public class SprintBootAnnotationExample {
+//    public @interface ValidPesel {
+//        String message() default "Niepoprawny numer PESEL"; // domyslny komunikat
+//        Class<?>[] groups() default {}; // uzywane w grupach walidacyjnych
+//        Class<? extends Payload>[] payload() default {}; // dodatkowe dane
+//    }
+}
