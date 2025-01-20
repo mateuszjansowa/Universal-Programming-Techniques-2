6aced792ba2fbc1c5d575a1e90ce24db9df7e0a6
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-01-20 21:06:20 +0100

    add interruptible task and interrupt it with shutdownNow

--- src/_08_Threads_Executors/Interrupt/InterruptExample.java
+++ src/_08_Threads_Executors/Interrupt/InterruptExample.java
@@ -1,4 +1,37 @@
 package _08_Threads_Executors.Interrupt;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class InterruptibleTask implements Runnable {
+    private String name;
+
+    public InterruptibleTask(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            if (Thread.currentThread().isInterrupted()) {
+                System.out.println(name + " is interrupted!");
+                return;
+            }
+            System.out.println(name);
+            Thread.yield(); // oddaje czas procesora
+        }
+    }
+}
+
 public class InterruptExample {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService exec = Executors.newFixedThreadPool(2);
+
+        for (int i = 1; i <= 2; i++) {
+            exec.execute(new InterruptibleTask("Task " + i));
+        }
+
+        Thread.sleep(1000);
+        exec.shutdownNow(); // ustawienie flagi isInterrupted
+    }
 }
