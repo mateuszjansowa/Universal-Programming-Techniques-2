cb2f519fe690e2ca5321a795337c02f9327046eb
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-01-26 17:33:09 +0100

    add callable example

--- src/_08_Threads_Executors/CallableAndFuture/CallableExample.java
+++ src/_08_Threads_Executors/CallableAndFuture/CallableExample.java
@@ -1,4 +1,34 @@
 package _08_Threads_Executors.CallableAndFuture;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class SquareCalculator implements Callable<Integer> {
+    private int number;
+
+    public SquareCalculator(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        Thread.sleep(1000); // symulacja dlugiej operacji
+        return (int) Math.pow(number, number);
+    }
+}
+
 public class CallableExample {
+    public static void main(String[] args) throws Exception {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        int number = 22;
+        Future<Integer> future = executor.submit(new SquareCalculator(number));
+
+        System.out.println("Calculating...");
+        int result = future.get();
+
+        System.out.println("Result of " + number + "^" + number + " = " + result);
+        executor.shutdown();
+    }
 }
