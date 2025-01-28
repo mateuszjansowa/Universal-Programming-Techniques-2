ca25e58aa7eb75c553bb6db39b673a216c6164b9
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-01-28 16:14:26 +0100

    finish task 02

--- src/_08_Threads_Executors/Challenges/Task_2.java
+++ src/_08_Threads_Executors/Challenges/Task_2.java
@@ -1,15 +1,45 @@
 package _08_Threads_Executors.Challenges;
 
-// 2. U?ycie ExecutorService do wykonania kilku zada?
-//Cel: Stwórz pul? 5 w?tków (FixedThreadPool) i uruchom 10 zada?, gdzie ka?de zadanie:
-//
-//Wypisuje swój numer (Zadanie X zaczyna prac?)
-//Usypia si? na losowy czas (100-1000 ms)
-//Ko?czy prac? (Zadanie X zako?czy?o prac?).
-//? Wskazówki:
-//
-//Wykorzystaj ExecutorService i Runnable.
-//U?yj Executors.newFixedThreadPool(5).
-//Zako?cz wykonawc? metod? shutdown() po uruchomieniu wszystkich zada?.
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Task_2 {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            exec.execute(new Task(i));
+        }
+
+        exec.shutdown();
+        try {
+            if (!exec.awaitTermination(5, TimeUnit.SECONDS)) {
+                System.out.println("Nie wszystkie zadania zakonczyly sie");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Task implements Runnable {
+    private final int id;
+    private final Random random = new Random();
+
+    public Task(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.printf("Zadanie #%d zaczyna prace\n", id);
+            Thread.sleep(random.nextInt(1000) + 100);
+            System.out.printf("Zadanie #%d zakonczylo prace\n", id);
+        } catch (InterruptedException e) {
+            System.out.printf("Zadanie #%d zosta?o przerwane!\n", id);
+            Thread.currentThread().interrupt();
+        }
+    }
 }
