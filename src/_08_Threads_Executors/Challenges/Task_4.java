63a50cf119b5b98dcf55b5d2c6e7aa8e843fce42
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-01-28 21:17:40 +0100

    finish task 04

--- src/_08_Threads_Executors/Challenges/Task_4.java
+++ src/_08_Threads_Executors/Challenges/Task_4.java
@@ -1,13 +1,68 @@
 package _08_Threads_Executors.Challenges;
 
-// Zadania ?rednio?atwe (2)
-//4. Równoczesne przetwarzanie du?ej tablicy liczb
-//Cel: Podziel tablic? int[] numbers = new int[1_000_000] na 4 cz??ci i równocze?nie policz sum? ka?dej cz??ci w osobnych w?tkach.
-//
-//? Wskazówki:
-//
-//U?yj ExecutorService i Callable<Integer>, by równocze?nie liczy? sumy fragmentów tablicy.
-//Na ko?cu zbierz wyniki Future<Integer> i policz sum? ca?o?ci.
-//Porównaj czas dzia?ania wersji jednordzeniowej (for + sum) i wielow?tkowej (ExecutorService).
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Task_4 {
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+        int[] numbers = new int[1_000_000];
+        int totalSum = 0;
+        ExecutorService exec = Executors.newFixedThreadPool(4);
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(10);
+        }
+
+        List<Callable<Integer>> tasks = List.of(
+                new Summator(numbers, 0, numbers.length / 4),
+                new Summator(numbers, numbers.length / 4, numbers.length / 2),
+                new Summator(numbers, numbers.length / 2, (numbers.length - numbers.length / 4)),
+                new Summator(numbers, (numbers.length - numbers.length / 4), numbers.length)
+        );
+
+        try {
+            List<Future<Integer>> results = exec.invokeAll(tasks);
+            for (Future<Integer> result : results) {
+                int partialSum = result.get();
+                System.out.printf("Result is %d\n", partialSum);
+                totalSum += partialSum;
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Execution was interrupted: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Unexpected error: " + e.getMessage());
+        } finally {
+            exec.shutdown();
+        }
+
+        System.out.printf("Total sum: %d\n", totalSum);
+    }
+}
+
+class Summator implements Callable<Integer> {
+    private final int[] arr;
+    private final int start, end;
+
+    public Summator(int[] arr, int start, int end) {
+        this.arr = arr;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        if (Thread.currentThread().isInterrupted()) {
+            return 0;
+        }
+
+        return Arrays.stream(arr, start, end).sum();
+    }
 }
