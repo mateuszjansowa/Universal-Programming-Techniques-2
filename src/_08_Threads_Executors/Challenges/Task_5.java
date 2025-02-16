fbe1836eff6c953b499ec8edfc5d0cbc056db7e0
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-02-16 19:46:05 +0100

    init task 05

--- src/_08_Threads_Executors/Challenges/Task_5.java
+++ src/_08_Threads_Executors/Challenges/Task_5.java
@@ -1,16 +1,4 @@
 package _08_Threads_Executors.Challenges;
 
-// invokeAll() – uruchamianie zbioru zada?
-//Cel: Masz list? 5 zada? Callable, które symuluj? pobieranie danych z API (czekaj? Thread.sleep(1000-5000) ms).
-//Program powinien:
-//
-//Uruchomi? wszystkie zadania równocze?nie (invokeAll).
-//Czeka? na wszystkie wyniki.
-//Wypisa? czas trwania ca?ego procesu i wyniki.
-//? Wskazówki:
-//
-//U?yj invokeAll() do równoczesnego uruchomienia zada?.
-//W Callable<String> zwracaj String, np. "Pobrano dane u?ytkownika X".
-//Sprawd? czas dzia?ania (System.nanoTime() lub System.currentTimeMillis()).
 public class Task_5 {
 }
