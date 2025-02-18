633f564cf0360a297d420b64bf7a025f66965b3b
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-02-18 21:42:27 +0100

    CountDownLatchExample

--- src/_09_Threads_Synchronizers/_04_CountdownLatch/CountDownLatchExample.java
+++ src/_09_Threads_Synchronizers/_04_CountdownLatch/CountDownLatchExample.java
@@ -1,4 +1,31 @@
 package _09_Threads_Synchronizers._04_CountdownLatch;
 
+import java.util.concurrent.CountDownLatch;
+
+class Worker extends Thread {
+    private final CountDownLatch latch;
+
+    public Worker(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void run() {
+        System.out.println(getName() + " finished work");
+        latch.countDown(); // obniza licznik
+    }
+}
+
 public class CountDownLatchExample {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(5);
+
+        new Worker(latch).start();
+        new Worker(latch).start();
+        new Worker(latch).start();
+        new Worker(latch).start();
+        new Worker(latch).start();
+
+        latch.await();
+        System.out.println("All threads finished working");
+    }
 }
