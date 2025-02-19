fad92139293206efcc5fe684ec7a12016c3e80ef
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-02-19 17:41:26 +0100

    Finish Task_02

--- src/_09_Threads_Synchronizers/Challenges/Task_02.java
+++ src/_09_Threads_Synchronizers/Challenges/Task_02.java
@@ -1,4 +1,43 @@
 package _09_Threads_Synchronizers.Challenges;
 
+class SharedResource {
+    private int data = 0;
+
+    public void update(int value) {
+        synchronized (this) {
+            int temp = data;
+            temp += value;
+            data = temp;
+        }
+    }
+
+    public int getData() {
+        return data;
+    }
+}
+
 public class Task_02 {
+    public static void main(String[] args) throws InterruptedException {
+        SharedResource resource = new SharedResource();
+
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                resource.update(1);
+            }
+        };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Main thread interrupted!");
+        }
+
+        System.out.println("Oczekiwany wynik: 2000, Aktualny wynik: " + resource.getData());
+    }
 }
